f1a903d0f13de0500b45750657126d1e596c72ec
binaryCoder-101 <syed.misak@gmail.com>
2026-05-15 00:20:00 +0530

    Implemented basic keyword search for movies
    - Load movies from data/movies.json
    - Filter movies by case-sensitive substring match on title
    - Print up to 5 results ordered by ascending ID

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -4,6 +4,9 @@ import json
 
 def main() -> None:
     json_file = open('data/movies.json') 
+    json_file_obj = json.load(json_file)
+
+
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
@@ -12,10 +15,17 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    result = []
+
+    for movie in json_file_obj["movies"]:
+        if args.query in movie["title"] and len(result) < 5:
+            result.append(movie)
+
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            pass
+            for movie in result:
+                print(f"- {movie['title']}")
         case _:
             parser.print_help()
 
